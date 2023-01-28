5e5367e0d094e54ac893e11a428b91913be45ee5
LsB.Dev <prof.luifel@gmail.com>
2023-01-27 22:38:06 -0300

    Insertion/displayToggle

--- js/script.js
+++ js/script.js
@@ -17,3 +17,44 @@ function changeTocreateQuizzSection() {
   document.querySelector('.quizz').classList.add('hidden');
 }
 
+// axios.get('https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes').then((resonse)=>{console.log(resonse.data)});
+
+function togglePergunta(element) {
+  element.classList.add('hidden');
+  element.nextElementSibling.classList.remove('hidden');
+
+}
+
+const perguntas = document.querySelectorAll(".start__question");
+
+for(let i = 0; i <perguntas.length; i++) {
+  perguntas[i].innerHTML = `
+    <label for="">Pergunta ${i + 1}</label>
+    <input type="text" placeholder="Texto da pergunta">
+    <input type="text" placeholder="Cor de fundo da pergunta">
+    <label for="">Resposta Correta</label>
+    <input type="text" placeholder="Resposta correta">
+    <input type="text" placeholder="URL da imagem">
+    <label for="">Respostas incorretas</label>
+    <input type="text" placeholder="Resposta incorreta 1">
+    <input type="text" placeholder="URL da imagem 1">
+    <br>
+    <input type="text" placeholder="Resposta incorreta 2">
+    <input type="text" placeholder="URL da imagem 2">
+    <br>
+    <input type="text" placeholder="Resposta incorreta 3">
+    <input type="text" placeholder="URL da imagem 3">
+    `;
+
+}
+
+
+
+// function postQuizz() {
+//   const criacaoPergunta = document.querySelectorAll('start__data').value;
+
+
+// }
+
+
+
