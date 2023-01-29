0cefb55133d38122ecb18c2c3cb7fbdf47e91f4a
Lucas Nunes <120993347+thezunes@users.noreply.github.com>
2023-01-28 22:43:18 -0300

    Exibição de todos os Quizzes

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
+let objetoQuizz = [];
 
+exibirQuizz();
 
 function changeToQuizzHub() {
   document.querySelector('.hub__quizz').classList.remove('hidden');
@@ -17,3 +19,38 @@ function changeTocreateQuizzSection() {
   document.querySelector('.quizz').classList.add('hidden');
 }
 
+
+function exibirQuizz(){
+
+const promise = axios.get("https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes", objetoQuizz)
+promise.then(exibiuQuizz);
+promise.catch(erroExibirQuizz);
+
+}
+
+function exibiuQuizz(resposta){
+
+const elementoQuizzes=document.getElementById("quizzes");
+console.log("deu certo");
+let qntQuizz = resposta.data.length;
+console.log(resposta.data);
+objetoQuizz = resposta.data;
+console.log(objetoQuizz)
+
+for (let j=0;j<6;j++){
+
+    elementoQuizzes.innerHTML+=
+    `<div class="quizzes" id="${resposta.data[j].id}">
+        <img class="image-quizz" src="${resposta.data[j].image}">
+        <h3 class="quizzes__title">${resposta.data[j].title}</h3>
+        </div>
+    </div>`
+}
+}
+
+
+function erroExibirQuizz(erro){
+
+
+
+}
